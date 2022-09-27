a40ff6ca090e39a36572b611f847668ae47194d4
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-09-27 20:00:24 +0200

    Initial commit
    Generate project using Ktor Generator

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.7.10"
+    id("io.ktor.plugin") version "2.1.1"
+                id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+}
+
+group = "com.dreamsoftware"
+version = "0.0.1"
+application {
+    mainClass.set("com.dreamsoftware.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.example.mfa_test"
