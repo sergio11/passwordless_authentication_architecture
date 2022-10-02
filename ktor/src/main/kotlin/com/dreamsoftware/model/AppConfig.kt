2318cc471dfad8f233d1b1ac92308a01e98af625
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-10-02 14:13:16 +0200

    Add Push OTP Sender

--- ktor/src/main/kotlin/com/dreamsoftware/model/AppConfig.kt
+++ ktor/src/main/kotlin/com/dreamsoftware/model/AppConfig.kt
@@ -10,6 +10,7 @@ data class OtpSenderConfig(
     val otpLength: Int,
     val useLetters: Boolean,
     val useDigits: Boolean,
-    val ttlMinutes: Int)
+    val ttlMinutes: Int,
+    val serviceKey: String)
 data class RedisClusterConfig(val nodes: List<RedisNodeConfig>)
 data class RedisNodeConfig(val host: String, val port: Int)
